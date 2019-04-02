ece04e234c8e3af7ea5d3f551b30340fffd4b473
prashant-accion <prashant.jaiswal@accionlabs.com>
2019-04-02 11:25:45 +0530

    Unwanted files deleted.

--- scripts/4_4_1_ts_const_var.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// export {};
-var firstName;
-//const firstName1; //error - const must have some value `while declaration/initialized`
-var lastName = 'Jayaswal';
-// const lastName //error - const value is fixed, `not to change/re-assigned` in future
-console.log(firstName);
-console.log(lastName);
-//# sourceMappingURL=4_4_1_ts_const_var.js.map
