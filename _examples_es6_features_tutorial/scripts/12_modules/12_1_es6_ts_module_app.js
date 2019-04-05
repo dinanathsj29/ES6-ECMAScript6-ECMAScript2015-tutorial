046961525899adc62f6fdba64a0d820ba4dc31fd
Dinanath Jayaswal <41334766+dinanathsj29@users.noreply.github.com>
2019-04-05 08:44:29 +0530

    examples uploaded - chapter 12-1 typescript
    examples uploaded - chapter 12-1 typescript

---
+++ _examples_es6_features_tutorial/scripts/12_modules/12_1_es6_ts_module_app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Import keyword makes variables/functions/classes ready to be used/access in current modules
+var _12_1_es6_ts_module_script_1 = require("./12_1_es6_ts_module_script");
+console.log(_12_1_es6_ts_module_script_1.fName);
+alert(_12_1_es6_ts_module_script_1.getAge(2019));
+// console.log('// ------------------------------');
+//# sourceMappingURL=12_1_es6_ts_module_app.js.map
---
+++ _examples_es6_features_tutorial/scripts/12_modules/12_1_es6_ts_module_script.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// define variables/functions to export
+var fName = 'Dinanath';
+exports.fName = fName;
+var DOBY = 1980;
+exports.DOBY = DOBY;
+function getAge(_curYear) {
+    console.log(_curYear - DOBY);
+}
+exports.getAge = getAge;
+// console.log('// ------------------------------');
+//# sourceMappingURL=12_1_es6_ts_module_script.js.map
